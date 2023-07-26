d2c7f11c4527a0d1893c53882788eece9bb50347
Iván Alexander Aráoz Andrade <69406064+HogRidee@users.noreply.github.com>
2023-07-26 14:37:04 -0500

    Add files via upload

---
+++ PC1/Adelanto/clase3/Adelanto - C3.R
@@ -0,0 +1,56 @@
+########################################
+### CLASE 3 - MEDIDAS DE POSICION   ####
+########################################
+
+# Ejercicio 4
+# media
+# Farmacia A
+nA<- 40
+mi<- c(4,8,12,16,20)
+fi<- c(10,15,7,5,3)
+x_bar_A<- sum(mi*fi)/nA
+x_bar_A
+
+MeA<- 6+4*(20-10)/15
+MeA
+
+MoA<- 6+4*5/(5+8)
+MoA
+
+# Farmacia B
+nB<- 20
+xB<- c(6,7,7,8,9,9,10,12,13,13,
+       14,14,14,14,15,16,17,17,19,20)
+x_bar_B<- mean(xB)
+x_bar_B
+
+MeB<- median(xB)
+MeB
+
+MoB<- 14
+MoB
+
+# Ejercicio 5
+# a)
+x<- 24*0.5/2.5
+x
+(x/120)*100
+
+# b)
+p15<- 7.5+2.5*(18-15)/25
+p15  
+
+# Ejercicio 1
+seq(from=1240,by=480,length.out=5)
+1240-480
+3160+480
+
+
+
+
+
+  
+  
+  
+
+
